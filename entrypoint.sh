73598381793edbe547fe229c89be7a5cc932876b
willjsw <sunwoo1137@gmail.com>
2025-06-11 17:49:23 +0900

    KW-474/chore: flag option -> config 변경

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,36 +1,15 @@
 #!/bin/bash
 
-aca-py start \
-  --endpoint http://192.168.0.82:8005 \
-  --inbound-transport http 0.0.0.0 8005 \
-  --inbound-transport ws 0.0.0.0 8020 \
-  --outbound-transport ws \
-  --outbound-transport http \
-  --admin 0.0.0.0 8002 \
-  --admin-insecure-mode \
-  --wallet-type askar \
-  --wallet-name base_wallet \
-  --wallet-key basekey \
-  --wallet-storage-type sqlite \
-  --wallet-storage-config '{"path":"/tmp/base_wallet.db"}' \
-  --multitenant \
-  --multitenant-admin \
-  --jwt-secret mysupersecret \
-  --auto-provision \
-  --label multitenant-aca \
-  --no-ledger \
-  --auto-accept-invites \
-  --auto-accept-requests \
-  --auto-respond-messages \
-  --auto-respond-credential-offer \
-  --auto-respond-credential-request \
-  --auto-respond-presentation-request \
-  --enable-undelivered-queue \
-  --emit-new-didcomm-prefix \
-  --open-mediation \
-  --log-level info &
+aca-py start --arg-file /configs/config.yml &
 
 PID=$!
+echo "ACA-Py started with PID: $PID"
+
+echo "Waiting for ACA-Py to be ready..."
 sleep 5
+
+echo "Running wallet initialization..."
 /init_wallets.sh
+
+echo "ACA-Py is running. Waiting for process to complete..."
 wait $PID
